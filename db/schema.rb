42d17df3441c944fd5c385791f06073160a6c520
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-19 00:39:41 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180719002815) do
+ActiveRecord::Schema.define(version: 20180719003904) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "password"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
-    t.string   "password_confirmation"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
